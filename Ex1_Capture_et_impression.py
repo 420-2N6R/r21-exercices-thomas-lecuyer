acda184a5dbabdf94b75d7be27848efd27c18dc0
Thomas L'Ecuyer <2365475@cegepmontpetit.ca>
2024-04-12 12:56:35 -0400

    progres

--- Ex1_Capture_et_impression.py
+++ Ex1_Capture_et_impression.py
@@ -6,17 +6,20 @@
 import os
 os.chdir(os.path.dirname(__file__))
 
-os.chdir("csvs")
+os.chdir("VIDEOS")
 
 for fichier in os.listdir():
-    nom_fichier, ext = os.path.splitext(fichier)
-    titre, cours, num = nom_fichier.split()
-    titre = titre.strip()
-    cours = cours.strip()
+    try:
+            nom_fichier, ext = os.path.splitext(fichier)
+            titre, cours, num = nom_fichier.split("_")
+            titre = titre.strip()
+            cours = cours.strip()
 
-    num = num.strip()
-    num = num[1:]   # retire le dièse juste avant le numéro de cours
-    num = num.zfill(2) # rajoute des 0 avec le numéro de cours pour obtenir un num de 2 caractères
+            num = num.strip()
+            num = num[1:]   # retire le dièse juste avant le numéro de cours
+            num = num.zfill(2) # rajoute des 0 avec le numéro de cours pour obtenir un num de 2 caractères
 
-    nouveau_nom = f"{num} {cours} {titre}{ext}"
-    os.rename(fichier, nouveau_nom)
+            nouveau_nom = f"{num} {cours} {titre}{ext}"
+            os.rename(fichier, nouveau_nom)
+    except ValueError:
+        print(f"Mauvais format de texte pour le fichier: {fichier}")
