ae061a6a76dba28d287dfd63a0ae88df808dbd28
Thomas L'Ecuyer <2365475@cegepmontpetit.ca>
2024-04-12 13:26:08 -0400

    progres

--- Ex2_LoggerLesErreurs.py
+++ Ex2_LoggerLesErreurs.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 
 # on veut lire les données dans les fichiers contenue dans le répertoire "Doc Text"
 # On veut construire une liste contenant les noms de toutes les personnes dans ces différents documents json.
@@ -12,6 +13,10 @@ os.chdir("Doc Text")
 list_fichier_groupes = ['gr2010.json', 'gr2060.json', 'gr2100.json', 'gr2200.json', 'gr2300.json', 'gr3460.json', 'gr2001.json']
 
 for fichier in list_fichier_groupes :
-    with open(fichier,"r") as f_lu :
-        texte = f_lu.read()
-        data = json.loads(texte)
+    try:
+        with open(fichier,"r",encoding="utf-8") as f_lu :
+            texte = f_lu.read()
+            data = json.loads(texte)
+    except Exception as ex:
+        with open("log_erreur.txt","a") as f_append:
+            f_append.write(f"{type(ex)} {ex} a {datetime.datetime.now()}\n")
